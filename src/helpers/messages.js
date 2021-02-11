faa2d378508d74ba1e5b5242874a68126935de7a
Stanley Ebuka <Chemicalstan15@Gmail.Com>
2021-02-11 13:14:12 +0100

    Define massages helper function

---
+++ src/helpers/messages.js
@@ -0,0 +1,15 @@
+const successRes = {
+  message: "",
+  status: "success",
+  data: null
+};
+const errorRes = {
+  message: "",
+  status: "error",
+  data: null
+};
+
+export default {
+  successRes,
+  errorRes
+};
