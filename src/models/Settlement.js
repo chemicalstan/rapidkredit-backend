234643a4a2f5d54eb193d91e51cd6d7197c0361d
Stanley Ebuka <Chemicalstan15@Gmail.Com>
2021-02-11 11:10:32 +0100

    Define settlement model json schema

--- src/models/Settlement.js
+++ src/models/Settlement.js
@@ -4,6 +4,20 @@ class Settlement extends Model {
     static get tableName(){
         return 'settlement'
     }
+
+    static get jsonSchema (){
+        return {
+            type: 'object',
+            required: ['employerId', 'staffId', 'paymentDueDate'],
+            properties: {
+                id: {type: 'uuid'},
+                employerId: {type: 'uuid'},
+                staffId: {type: 'uuid'},
+                isSettled: {type: 'boolean'},
+                paymentDueDate: {type: 'date'},
+            }
+        }
+    }
 }
 
 export default Settlement;
