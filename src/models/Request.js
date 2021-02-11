489f034f6723427cdc5561ae21e0f5ce87d06c34
Stanley Ebuka <Chemicalstan15@Gmail.Com>
2021-02-11 11:10:08 +0100

    Define request model json schema

--- src/models/Request.js
+++ src/models/Request.js
@@ -1,20 +1,35 @@
 import { Model } from "objection";
+import { type } from "os";
 
 class Request extends Model {
-    static get tableName(){
-        return 'request'
-    }
-    static get relationMappings (){
-        import Staff from './Staff';
-        return {
-            relation: Model.BelongsToOneRelation,
-            modelClass: Staff,
-            join: {
-                from: 'request.staff_id',
-                to: 'staff.id'
-            }
-        }
-    }
+  static get tableName() {
+    return "request";
+  }
+  static get jsonSchema() {
+    return {
+      type: "object",
+      required: ["staffId", "amount", "requestStatus"],
+      properties: {
+        id: { type: "uuid" },
+        staffId: { type: "uuid" },
+        requestTime: { type: "datetime" },
+        amount: { type: "decimal" },
+        requestStatus: { type: "string" },
+        updatedAt: { type: "timestamp" }
+      }
+    };
+  }
+  static get relationMappings() {
+    import Staff from "./Staff";
+    return {
+      relation: Model.BelongsToOneRelation,
+      modelClass: Staff,
+      join: {
+        from: "request.staff_id",
+        to: "staff.id"
+      }
+    };
+  }
 }
 
 export default Request;
