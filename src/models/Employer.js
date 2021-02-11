6eaea278512bca8e03b10cac671cdbad5b6e6276
Stanley Ebuka <Chemicalstan15@Gmail.Com>
2021-02-11 11:09:42 +0100

    Define employer json schema

--- src/models/Employer.js
+++ src/models/Employer.js
@@ -1,28 +1,52 @@
 import { Model } from "objection";
 
 class Employer extends Model {
-    static get tableName(){
-        return 'employer'
-    }
-
-    static get relationMappings () {
-        import Staff from "./Staff";
-        return {
-            // Relating employer to their staffs
-            Staffs: {
-                relation: Model.ManyToManyRelation,
-                modelClass: Staff,
-                join: {
-                    from: 'employer.id',
-                    through: {
-                        from: 'employer_staff.employer_id',
-                        to: 'employer_staff.staff_id'
-                    },
-                    to: 'staff.id'
-                }
-            }
+  static get tableName() {
+    return "employer";
+  }
+  static get jsonSchema() {
+    return {
+      type: "object",
+      required: [
+        "firstName",
+        "lastName",
+        "password",
+        "phone",
+        "email",
+        "companyName"
+      ],
+      properties: {
+        id: { type: "uuid" },
+        firstName: { type: "string" },
+        lastName: { type: "string", minLength: 1, maxLength: 255 },
+        password: { type: "string", minLength: 1, maxLength: 255 },
+        phone: { type: "integer" },
+        email: { type: "string", minLength: 1, maxLength: 255 },
+        emmployerImageUrl: { type: ["string", "null"] },
+        companyName: { type: "string", minLength: 1, maxLength: 255 },
+        companyEmployeeApi: { type: "string", minLength: 1, maxLength: 255 },
+        updatedAt: { type: "timestamp" }
+      }
+    };
+  }
+  static get relationMappings() {
+    import Staff from "./Staff";
+    return {
+      // Relating employer to their staffs
+      Staffs: {
+        relation: Model.ManyToManyRelation,
+        modelClass: Staff,
+        join: {
+          from: "employer.id",
+          through: {
+            from: "employer_staff.employer_id",
+            to: "employer_staff.staff_id"
+          },
+          to: "staff.id"
         }
-    }
+      }
+    };
+  }
 }
 
 export default Employer;
