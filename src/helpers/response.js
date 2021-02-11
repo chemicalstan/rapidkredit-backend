31941e07fa064f6a92eef5363d497d48a6b1b28f
Stanley Ebuka <Chemicalstan15@Gmail.Com>
2021-02-11 13:14:58 +0100

    Define helper functions

---
+++ src/helpers/response.js
@@ -0,0 +1,19 @@
+import { status } from './status';
+/**
+ *  Success (200)
+ * @param {objObjectect} messageObj message object
+ * @param {Object} res response object
+ * @returns {Object} OK
+ */
+const success =  (messageObj, res) => {
+    return res.status(status.success).json(messageObj);
+  };
+  /**
+   * Bad Response (400)
+   * @param {Object} messageObj message object
+   * @param {Object} res response object
+   * @returns {Object} Bad Response
+   */
+  const error = (messageObj, res) => {
+    return res.status(status.bad).json(messageObj);
+  };
