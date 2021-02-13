6469d6db554d1511d43b5cc6feb994fcf99d46aa
Stanley Ebuka <Chemicalstan15@Gmail.Com>
2021-02-13 19:17:23 +0100

    Expose controllers

---
+++ src/controllers/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    authController: require('./authController'),
+    BankDetailController: require('./bankDetailController'),
+    requestController: require('./requestController'),
+    userController: require('./userController'),
+}
