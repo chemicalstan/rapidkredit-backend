216c9e39e96ed36fdbac35f9103352c98efe3ab7
Stanley Ebuka <Chemicalstan15@Gmail.Com>
2021-02-13 19:18:40 +0100

    Expose bank endpoints

--- src/routes/bankDetailRoutes.js
+++ src/routes/bankDetailRoutes.js
@@ -1,7 +1,24 @@
 const {Router} = require('express');
 
-const routes = Router();
+const router = Router();
 
-routes.get('/bank-details', (req, res)=>{
-    return res.status(200).json({message: 'Welcome to bank-details page'})
-})
+router.get('/', (req, res)=>{
+    return res.status(200).json({message: 'get all bank details'})
+});
+
+router.get('/:bankId', (req, res)=>{
+    return res.status(200).json({message: 'get bank details by id'})
+});
+
+router.post('/', (req, res)=>{
+    return res.status(200).json({message: 'add new bank details'})
+});
+
+router.patch('/:bankId', (req, res)=>{
+    return res.status(200).json({message: 'edit bank details'})
+});
+router.delete('/:bankId', (req, res)=>{
+    return res.status(200).json({message: 'delete bank details'})
+});
+
+module.exports = router;
