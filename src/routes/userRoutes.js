a6ac44cdb81c994227cf84be849d3b7f5c8fd99d
Stanley Ebuka <Chemicalstan15@Gmail.Com>
2021-02-13 19:18:20 +0100

    Expose user endpoints

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,21 @@
-import {Router} from 'express';
-const router = Router()
+const {Router} = require('express');
+const router = Router();
 
-router.get('/users', (req, res)=>{
-    res.status(200).json({message: 'get all users'})
+router.get('/', (req, res)=>{
+    return res.status(200).json({message: 'get all users from users table'});
 })
 
-router.post('/user')
+router.get('/:userId', (req, res)=>{
+    return res.status(200).json({message: 'get user from users table by id'});
+})
+
+router.patch('/:userId', (req, res)=>{
+    return res.status(200).json({message: 'update a user'});
+})
+
+router.delete('/:userId', (req, res)=>{
+    return res.status(200).json({message: 'delete a user'});
+})
+
+
+module.exports = router;
