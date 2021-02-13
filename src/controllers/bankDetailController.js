6eadfcf4444f31b0a667d20170e566de2a5e21fd
Stanley Ebuka <Chemicalstan15@Gmail.Com>
2021-02-13 19:16:12 +0100

    Initiate bank controller class  with action methods

---
+++ src/controllers/bankDetailController.js
@@ -0,0 +1,35 @@
+import BankDetail from "../../dist/models/BankDetail";
+module.exports = class BankDetail {
+  /**
+   * Add account
+   * @param {Object} req
+   * @param {Object} res
+   * @param {Object} next
+   * @returns {Object} Bank Detail Object
+   */
+  static async add(req, res, next) {
+      const {accountName, bankName, accountNumber} = req.body;
+      const {id} = req.user;
+    return res.status(200).send({ message: "get all bank details" });
+  }
+  /**
+   * Get all bank details
+   * @param {Object} req
+   * @param {Object} res
+   * @param {Object} next
+   * @returns {Object} Bank Detail Objects
+   */
+  static async getAll(req, res, next) {
+    return res.status(200).send({ message: "get all bank details" });
+  }
+  /**
+   * Get bank detail by id
+   * @param {Object} req
+   * @param {Object} res
+   * @param {Object} next
+   * @returns {Object} Bank Detail Object
+   */
+  static async getOne(req, res, next) {
+    return res.status(200).send({ message: "get bank detail by id" });
+  }
+};
